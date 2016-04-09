c9121cbdd372279d26613846ff36190ebbb48cf4
Willy DONGMO <w.dongmo@gmail.com>
2016-04-09 10:33:21 +0200

    chore

---
+++ data.js
@@ -0,0 +1,47 @@
+var family={
+  "family": [
+    {
+      "id": 1,
+      "firstName": "Sorelle",
+      "lastName": "DONGMO",
+      "dob":new Date(1987,5,12).toString()
+    },
+    {
+      "id": 2,
+      "firstName": "Cathi",
+      "lastName": "SONFACK",
+      "dob":new Date(1989,5,7).toString()
+    },
+    {
+      "id": 3,
+      "firstName": "Willy",
+      "lastName": "DONGMO",
+      "dob":new Date(1991,2,26).toString()
+    },
+    {
+      "id": 4,
+      "firstName": "Astride",
+      "lastName": "AWOUNBENG",
+      "dob":new Date(1993,9,26).toString()
+    },
+    {
+      "id": 5,
+      "firstName": "Gabine",
+      "lastName": "ZEKENG",
+      "dob":new Date(1997,2,26).toString()
+    },
+    {
+      "id": 6,
+      "firstName": "McJeryl",
+      "lastName": "TSAFA",
+      "dob":new Date(2005,11,10).toString()
+    },
+    {
+      "id": 7,
+      "firstName": "Renada",
+      "lastName": "AWOUNBENG",
+      "dob":new Date(2002,9,26).toString()
+    }
+  ]
+}
+module.exports=family;
---
+++ test.js
@@ -0,0 +1,58 @@
+var Excel = require('exceljs');
+var data =require('./data');
+var _ = require('lodash');
+// construct a streaming XLSX workbook writer with styles and shared strings
+var options = {
+    filename: './streamed-workbook.xlsx',
+    useStyles: true,
+    useSharedStrings: true
+};
+var workbook = new Excel.stream.xlsx.WorkbookWriter(options);
+var worksheet = workbook.addWorksheet('Feuille 1','FF0000');
+
+worksheet.columns =[
+	{header:'Id',key:'id',width:10},//A
+	{header:'First Name',key:'firstName',width:32},//B
+	{header:'Last Name',key:'lastName',width:32},//C
+	{header:'Birth Date',key:'dob',width:43}//D
+];
+data.family.forEach(function(elt){
+	//console.log(elt);
+	worksheet.addRow(elt);
+});
+var firstNameCol = worksheet.getColumn('firstName');
+var lastNameCol=worksheet.getColumn('lastName');
+var dobCol = worksheet.getColumn('dob');
+var firstName=[];
+var lastName=[];
+var dob=[];
+var workbookObj =[];
+
+firstNameCol.eachCell(function(cell, rowNumber){
+	if(rowNumber){
+			firstName[rowNumber-1]=cell.value;
+	}
+
+});
+lastNameCol.eachCell(function(cell,rowNumber){
+	if(rowNumber){
+				lastName[rowNumber-1]=cell.value;
+	}
+});
+dobCol.eachCell(function(cell,rowNumber){
+	if(rowNumber){
+				dob[rowNumber-1]=cell.value;
+	}
+});
+firstName=_.without(firstName,firstNameCol.header);
+lastName =_.without(lastName, lastNameCol.header);
+dob =_.without(dob, dobCol.header);
+firstName.forEach(function(elt, index){
+	workbookObj.push({firstName:elt,lastName:lastName[index], dob:dob[index]});
+});
+
+console.log(JSON.stringify(workbookObj,2));
+
+workbook.commit();
+
+
