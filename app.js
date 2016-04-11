9e8c233e31aecb22c046157e7503e61bd70aab09
Willy DONGMO <w.dongmo@gmail.com>
2016-04-11 13:52:57 +0200

    chore

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var morgan = require('morgan');
 var upload = multer({dest:'./uploads/'});
 var bodyParser = require('body-parser');
 var Excel = require('exceljs');
-
 var app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname+'/client/app'));
 app.use(bodyParser.json());
+var _ = require('lodash');
+var extractData = require('./test');
 
 var workbook = new Excel.Workbook();
 
@@ -18,12 +19,15 @@ app.get('/',function(req, res, next){
 })
 app.post('/',upload.single('workbook'),function(req, res){
 	res.status(200).end('Everything OK');
+	//app.filename = req.file.filename;
 	workbook.xlsx.readFile(__dirname+'/uploads/'+req.file.filename)
     .then(function() {
-    	
+    	extractData(workbook);
     });
 });
+
 
 app.listen(8000, function(){
 	console.log('app is listening on port 8000');
 });
+
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-var Excel = require('exceljs');
-var data =require('./data');
+//var Excel = require('exceljs');
+//var data =require('./data');
 var _ = require('lodash');
+
+
 // construct a streaming XLSX workbook writer with styles and shared strings
-var options = {
+/*var options = {
     filename: './streamed-workbook.xlsx',
     useStyles: true,
     useSharedStrings: true
@@ -19,40 +21,53 @@ worksheet.columns =[
 data.family.forEach(function(elt){
 	//console.log(elt);
 	worksheet.addRow(elt);
-});
-var firstNameCol = worksheet.getColumn('firstName');
-var lastNameCol=worksheet.getColumn('lastName');
-var dobCol = worksheet.getColumn('dob');
-var firstName=[];
-var lastName=[];
-var dob=[];
-var workbookObj =[];
+});*/
 
-firstNameCol.eachCell(function(cell, rowNumber){
-	if(rowNumber){
-			firstName[rowNumber-1]=cell.value;
-	}
+var extractDataFromFile = function(workbook){
+			var worksheet = workbook.addWorksheet('Feuille 1','FF0000');
+			worksheet.columns =[
+	{header:'Id',key:'id',width:10},//A
+	{header:'First Name',key:'firstName',width:32},//B
+	{header:'Last Name',key:'lastName',width:32},//C
+	{header:'Birth Date',key:'dob',width:43}//D
+];
+			var firstNameCol = worksheet.getColumn('firstName');
+			var lastNameCol=worksheet.getColumn('lastName');
+			var dobCol = worksheet.getColumn('dob');
+			var firstName=[];
+			var lastName=[];
+			var dob=[];
+			var workbookObj =[];
 
-});
-lastNameCol.eachCell(function(cell,rowNumber){
-	if(rowNumber){
-				lastName[rowNumber-1]=cell.value;
-	}
-});
-dobCol.eachCell(function(cell,rowNumber){
-	if(rowNumber){
-				dob[rowNumber-1]=cell.value;
-	}
-});
-firstName=_.without(firstName,firstNameCol.header);
-lastName =_.without(lastName, lastNameCol.header);
-dob =_.without(dob, dobCol.header);
-firstName.forEach(function(elt, index){
-	workbookObj.push({firstName:elt,lastName:lastName[index], dob:dob[index]});
-});
+			firstNameCol.eachCell(function(cell, rowNumber){
+				if(rowNumber){
+						firstName[rowNumber-1]=cell.value;
+				}
 
-console.log(JSON.stringify(workbookObj,2));
+			});
+			lastNameCol.eachCell(function(cell,rowNumber){
+				if(rowNumber){
+							lastName[rowNumber-1]=cell.value;
+				}
+			});
+			dobCol.eachCell(function(cell,rowNumber){
+				if(rowNumber){
+							dob[rowNumber-1]=cell.value;
+				}
+			});
+			firstName=_.without(firstName,firstNameCol.header);
+			lastName =_.without(lastName, lastNameCol.header);
+			dob =_.without(dob, dobCol.header);
+			firstName.forEach(function(elt, index){
+				workbookObj.push({firstName:elt,lastName:lastName[index], dob:dob[index]});
+			});
 
-workbook.commit();
+			console.log(JSON.stringify(workbookObj,2));
+
+			//workbook.commit();
+}
 
 
+/*http://alexperry.io/node/2015/03/25/promises-in-node.html*/
+
+module.exports = extractDataFromFile;
